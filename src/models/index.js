39eddabeb32bff286aa668a28e7758e60ad32c54
Jacob <jbale444@outlook.com>
2024-09-12 09:13:17 +0100

    Added migration to create a connection table for a reading list joining users and blogs for exercise 13.19

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,18 @@
 const Blog = require('./blog')
 const User = require('./user')
+const ReadingList = require('./readingList')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+User.belongsToMany(Blog, { through: ReadingList })
+Blog.belongsToMany(User, { through: ReadingList })
 
 // Blog.sync({ alter: true })
 // User.sync({ alter: true })
 
 module.exports = {
     Blog,
-    User
+    User,
+    ReadingList
 }
